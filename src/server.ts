b6333e05ee045f2dde0e52cd8351acc68eb9a6f3
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-07-30 23:34:02 +0200

    Configure env variables

--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import express from "express";
+import "dotenv/config";
 const app = express();
-const port = 4000;
+const port = process.env.PORT;
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`));
