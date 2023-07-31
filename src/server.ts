47e262b5a9c467c930c73bc66d4e5d9a389fb5b4
Michał Bysiewicz <46355817+mb-dir@users.noreply.github.com>
2023-07-31 13:13:16 +0200

    SN-5_create-user-model-and-schema (#2)
    * Create User model
    
    * Create mock controller and route
    
    Needed to configure postman

--- src/server.ts
+++ src/server.ts
@@ -1,11 +1,14 @@
 import express from "express";
 import "dotenv/config";
 import mongoose from "mongoose";
+import { getAllUsers } from "./controllers/userController";
 const app = express();
 const port = process.env.PORT;
 const DB_URI = process.env.DB_URI || "";
 
 app.use(express.json());
+
+app.use("/", getAllUsers);
 
 mongoose
   .connect(DB_URI)
