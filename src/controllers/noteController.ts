c43cfef128041e22660ba7b9d094ca36536c3054
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2023-08-26 21:25:38 +0200

    Add missed types

--- src/controllers/noteController.ts
+++ src/controllers/noteController.ts
@@ -34,12 +34,12 @@ export const getAllNotes = async (req: Request, res: Response) => {
         NoteModel.countDocuments(queryFilter),
       ]);
 
-    const notesOnCurrentPage = notes.length;
+    const notesOnCurrentPage: number = notes.length;
 
     return res
       .status(200)
       .json({ notes, totalNotesCount, page, notesPerPage, notesOnCurrentPage });
-  } catch (error) {
+  } catch (error: any) {
     return res.status(500).json({ error });
   }
 };
@@ -64,9 +64,13 @@ export const createNote = async (req: Request, res: Response) => {
         .status(400)
         .json({ error: "You have to specify the priority of the note" });
     }
-    const newNote = await NoteModel.create({ title, content, isHighPriority });
+    const newNote: INoteDocument = await NoteModel.create({
+      title,
+      content,
+      isHighPriority,
+    });
     return res.status(200).json({ newNote });
-  } catch (error) {
+  } catch (error: any) {
     return res.status(500).json({ error });
   }
 };
@@ -81,14 +85,16 @@ export const deleteNote = async (
       return res.status(400).json({ error: "Invalid note ID format" });
     }
 
-    const deletedNote = await NoteModel.findByIdAndDelete(id);
+    const deletedNote: INoteDocument | null = await NoteModel.findByIdAndDelete(
+      id
+    );
 
     if (!deletedNote) {
       return res.status(404).json({ error: "Note not found" });
     }
 
     return res.status(200).json({ deletedNote });
-  } catch (error) {
+  } catch (error: any) {
     return res.status(500).json({ error });
   }
 };
@@ -123,18 +129,21 @@ export const updateNote = async (
         .json({ error: "You have to specify the priority of the note" });
     }
 
-    const oldNote = await NoteModel.findByIdAndUpdate(id, {
-      title,
-      content,
-      isHighPriority,
-    });
+    const oldNote: INoteDocument | null = await NoteModel.findByIdAndUpdate(
+      id,
+      {
+        title,
+        content,
+        isHighPriority,
+      }
+    );
 
     if (!oldNote) {
       return res.status(404).json({ error: "Note not found" });
     }
 
     return res.status(200).json({ oldNote });
-  } catch (error) {
+  } catch (error: any) {
     return res.status(500).json({ error });
   }
 };
