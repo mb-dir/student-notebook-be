1d6f35e92c8d30f8bb0f946919f4be1a5c868f2e
Michał Bysiewicz <michal.bysiewicz.kontakt@gmail.com>
2024-01-08 22:12:12 +0100

    Working on creating todo

--- src/controllers/todoController.ts
+++ src/controllers/todoController.ts
@@ -8,5 +8,28 @@ export const createTodo = async (
   req: Request<{ user: IUserDocument }>,
   res: Response
 ) => {
-  return res.status(200).json({ msg: "It works" });
+  try {
+    const { content, endDate }: ITodoDocument = req.body;
+    const user_id: string | null = req?.user?._id;
+
+    if (typeof content !== "string" || content.trim() === "") {
+      return res
+        .status(400)
+        .json({ error: "Content must be a non-empty string" });
+    }
+    // improve it
+    if (typeof content !== "string") {
+      return res.status(400).json({ error: "End date must be a date" });
+    }
+
+    const newTodo: ITodoDocument = await TodoModel.create({
+      content,
+      endDate,
+      user_id,
+    });
+
+    return res.status(200).json(newTodo);
+  } catch (error: any) {
+    return res.status(500).json(error);
+  }
 };
